7f21d66f86d17ec2b0127c27e8cf88fdd012e41b
Matthew Poon <mpoon2@athabasca.edu>
2022-08-09 23:24:28 -0400

    added fontawesome react package, keycloak init

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,27 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from "react"
+import { ReactKeycloakProvider } from "@react-keycloak/web"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSpinner } from "@fortawesome/free-solid-svg-icons"
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
+const Loading = () => (
+  <div className="text-center py-8 block">
+    <p>
+      <FontAwesomeIcon icon={faSpinner} spinPulse size="xl" />
+    </p>
+    <p>Initializing server size rendering...</p>
+  </div>
+)
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ReactKeycloakProvider
+      authClient={{}} //an empty object instead of the keycloak instance for the static HTML pages
+      initOptions={{
+        onLoad: "login-required",
+      }}
+      LoadingComponent={<Loading />}
+    >
+      {element}
+    </ReactKeycloakProvider>
+  )
 }
