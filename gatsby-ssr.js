cfee8949c899904a73d8490d99f5d01e1ec3fbb4
Matthew Poon <mpoon2@athabasca.edu>
2022-08-10 22:21:32 -0400

    implement web font load optimization for typekit bely

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -53,7 +53,19 @@ const HtmlAttributes = {
  */
 const HeadComponents = [
   // Bely
+  <link rel="preconnect" href="https://use.typekit.net" crossorigin />,
+  <link rel="preconnect" href="https://p.typekit.net" crossorigin />,
+  <link rel="preload" as="style" href="https://use.typekit.net/dsn0wzu.css" />,
   <link rel="stylesheet" href="https://use.typekit.net/dsn0wzu.css" />,
+  <link
+    rel="stylesheet"
+    href="https://use.typekit.net/dsn0wzu.css"
+    media="print"
+    onload="this.media='all'"
+  />,
+  <noscript>
+    <link rel="stylesheet" href="https://use.typekit.net/dsn0wzu.css" />
+  </noscript>,
   // Source Han Sans
   <Script
     id="tp-import"
