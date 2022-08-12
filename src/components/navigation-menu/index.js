1f5125e9531e9d0a45f224098128d1ac0f57b058
Matthew Poon <mpoon2@athabasca.edu>
2022-08-11 22:36:38 -0400

    revert radix, switch back to tailwind

---
+++ src/components/navigation-menu/index.js
@@ -0,0 +1,8 @@
+/**
+ * Forward all exports.
+ * Pattern inspired by Josh W. Comeau
+ * Ref: https://www.joshwcomeau.com/react/file-structure/
+ */
+
+export * from "./navigation-menu"
+export { default } from "./navigation-menu"
