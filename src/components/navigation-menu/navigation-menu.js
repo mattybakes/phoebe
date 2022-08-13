b5398434605b1209068ab37533321cd77c7366d8
Matthew Poon <mpoon2@athabasca.edu>
2022-08-12 23:56:25 -0400

    mobile navbar changes

--- src/components/navigation-menu/navigation-menu.js
+++ src/components/navigation-menu/navigation-menu.js
@@ -11,7 +11,12 @@ import { Disclosure, Menu, Transition } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
 import { useKeycloak } from "@react-keycloak/web"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUserAstronaut, faLock } from "@fortawesome/free-solid-svg-icons"
+import {
+  faUserAstronaut,
+  faLock,
+  faBars,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons"
 import Logo from "./logo.inline.svg"
 import ChineseLogo from "./logo.chinese.inline.svg"
 import DarkToggle from "../darkmode-toggle"
@@ -90,12 +95,12 @@ export default function Navbar() {
           <div className="relative flex items-center justify-between">
             <div className="inset-y-0 left-0 flex items-center sm:hidden">
               {/* Mobile menu button*/}
-              <Disclosure.Button className="mobile-nav-button inline-flex items-center justify-center p-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <Disclosure.Button className="mobile-nav-button inline-flex items-center justify-center rounded-md h-14">
                 <span className="sr-only">Open main menu</span>
                 {open ? (
-                  <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  <FontAwesomeIcon icon={faXmark} size="s" aria-hidden="true" />
                 ) : (
-                  <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  <FontAwesomeIcon icon={faBars} size="s" aria-hidden="true" />
                 )}
               </Disclosure.Button>
             </div>
@@ -110,7 +115,7 @@ export default function Navbar() {
                   />
                   <ChineseLogo
                     className={
-                      "chinese-logo hidden sm:block my-auto pt-[0.25rem] ml-7 " +
+                      "chinese-logo hidden md:block my-auto pt-[0.25rem] ml-7 " +
                       (isShrunk ? "scale-down-logo" : "scale-up-logo")
                     }
                   />
@@ -189,14 +194,14 @@ export default function Navbar() {
                   leaveFrom="transform opacity-100 scale-100"
                   leaveTo="transform opacity-0 scale-95"
                 >
-                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Items className="origin-top-right absolute right-0 mt-3.5 w-48 py-3.5 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
                       {({ active }) => (
                         <a
                           href="#"
                           className={classNames(
                             active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
+                            "block px-3.5 text-sm text-gray-700"
                           )}
                         >
                           Profile
@@ -210,7 +215,7 @@ export default function Navbar() {
                             onClick={() => keycloak.logout()}
                             className={classNames(
                               active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-3.5 text-sm text-gray-700"
                             )}
                           >
                             Sign out
@@ -224,7 +229,7 @@ export default function Navbar() {
                             onClick={() => keycloak.login()}
                             className={classNames(
                               active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-3.5 text-sm text-gray-700"
                             )}
                           >
                             Sign in
@@ -240,7 +245,7 @@ export default function Navbar() {
 
           {/* Mobile Menu Dropdown */}
           <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+            <div className="space-y-3.5">
               {keycloak &&
                 keycloak.hasResourceRole("viewers") &&
                 navigationPrivate.map(item => (
@@ -250,9 +255,9 @@ export default function Navbar() {
                     href={item.href}
                     className={classNames(
                       item.current
-                        ? "bg-gray-900 text-white mobile-nav-item-active"
-                        : "mobile-nav-item hover:bg-gray-700 hover:text-white",
-                      "block px-3 py-2 rounded-md text-base font-medium"
+                        ? "bg-gray-900 text-white color-change-text hover:color-change-text-hover"
+                        : "mobile-nav-item color-change-text hover:color-change-text-hover",
+                      "block rounded-md font-medium"
                     )}
                     aria-current={item.current ? "page" : undefined}
                   >
@@ -268,9 +273,9 @@ export default function Navbar() {
                     href={item.href}
                     className={classNames(
                       item.current
-                        ? "bg-gray-900 text-white mobile-nav-item-active"
-                        : "mobile-nav-item hover:bg-gray-700 hover:text-white",
-                      "block px-3 py-2 rounded-md text-base font-medium"
+                        ? "bg-gray-900 text-white color-change-text hover:color-change-text-hover"
+                        : "mobile-nav-item color-change-text hover:color-change-text-hover",
+                      "block rounded-md font-medium"
                     )}
                     aria-current={item.current ? "page" : undefined}
                   >
