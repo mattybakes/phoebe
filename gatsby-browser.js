c0c065e54f5e77b8d824147cbe4fc4c2e8d1615f
Matthew Poon <mpoon2@athabasca.edu>
2022-08-20 12:40:34 -0400

    add sofia pro

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,11 @@
 // Fontsource typefaces
 import "@fontsource/poppins"
 import "@fontsource/ubuntu-mono"
+// @font face definitions for local fonts
+import "./src/typography.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
-
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
