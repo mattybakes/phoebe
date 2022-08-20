5bc070764f15c71b7ed3b98cb33689c189c23e64
Matthew Poon <mpoon2@athabasca.edu>
2022-08-20 17:23:06 -0400

    full keycloak

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+import React from "react"
+import { ReactKeycloakProvider } from "@react-keycloak/web"
+import keycloak from "./src/components/keycloak"
+
 // Fontsource typefaces
 import "@fontsource/poppins/latin-ext.css"
 import "@fontsource/ubuntu-mono/latin-ext.css"
@@ -9,3 +13,25 @@ import "./src/normalize.css"
 import "./src/style.css"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+/**
+ * Initialization spinner component
+ * @returns div containing a loader
+ */
+const Loading = () => (
+  <div className="init">
+    <p>Loading Keycloak...</p>
+  </div>
+)
+
+/**
+ * Wrap a Keycloak provided component around a page Element for authentication.
+ * @returns element wrapped in an Keycloak provider
+ */
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ReactKeycloakProvider authClient={keycloak} LoadingComponent={<Loading />}>
+      {element}
+    </ReactKeycloakProvider>
+  )
+}
