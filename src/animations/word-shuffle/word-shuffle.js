2e35304ce03e08e9668a40187b79177b3490f5f2
Matthew Poon <mpoon2@athabasca.edu>
2022-08-22 17:20:33 -0400

    added random text reveal animation

---
+++ src/animations/word-shuffle/word-shuffle.js
@@ -0,0 +1,146 @@
+import React from "react"
+import { useState } from "react"
+import { RandomReveal } from "react-random-reveal"
+import "./word-shuffle.scss"
+
+export default function WordShuffle(props) {
+  /* Detect mouse hover for resolution > 768 */
+  const [isHover, setIsHover] = useState(false)
+  const onMouseEnter = () => {
+    window.innerWidth > 768 && setIsHover(true)
+  }
+  const onMouseLeave = () => {
+    window.innerWidth > 768 && setIsHover(false)
+  }
+
+  /* Delay used to trigger caret */
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+  /* Custom Defaults for react-random-reveal */
+  const { isPlaying = true } = props
+  const { text = "Default" } = props
+  const { duration = 0.4 } = props
+  const { updateInterval = 0 } = props
+  const { revealDuration = 1 } = props
+  const { revealEasing = "linear" } = props
+  const {
+    characterSet = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+      "~",
+      "`",
+      "!",
+      "@",
+      "#",
+      "$",
+      "%",
+      "^",
+      "&",
+      "*",
+      "(",
+      ")",
+      "_",
+      "-",
+      "+",
+      "=",
+      "{",
+      "[",
+      "}",
+      "]",
+      "|",
+      "\\",
+      ":",
+      ";",
+      '"',
+      "'",
+      "<",
+      ",",
+      ">",
+      ".",
+      "?",
+      "/",
+    ],
+  } = props
+  const { ignoreCharacterSet } = props
+  return (
+    <span
+      className={isHover ? "shuffle slide" : "shuffle"}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {isHover ? (
+        <RandomReveal
+          isPlaying={isPlaying}
+          characters={text}
+          duration={duration}
+          updateInterval={updateInterval}
+          revealDuration={revealDuration}
+          revealEasing={revealEasing}
+          characterSet={characterSet}
+          ignoreCharacterSet={ignoreCharacterSet}
+          onComplete={() => ({ shouldRepeat: false, delay: 0 })}
+        />
+      ) : (
+        `${text}`
+      )}
+    </span>
+  )
+}
--- src/components/left-sidebar/left-sidebar.js
+++ src/components/left-sidebar/left-sidebar.js
@@ -1,16 +1,27 @@
 import React from "react"
-import { links } from "./links"
+import { useState } from "react"
 import { Link } from "gatsby"
+import { links } from "./links"
 import Xmark from "./xmark.inline.svg"
+import Shuffle from "../../animations/word-shuffle"
 import "./left-sidebar.scss"
 
 export const LeftSidebar = props => {
   const { isNavExpanded, setIsNavExpanded } = props
   const { closeNav } = props
+  const [isHover, setIsHover] = useState(false)
+
+  /* Detect mouse hover for resolution > 768 */
+  const onMouseEnter = () => {
+    window.innerWidth > 768 && setIsHover(true)
+  }
+  const onMouseLeave = () => {
+    window.innerWidth > 768 && setIsHover(false)
+  }
 
   return (
     <nav className="left-sidebar-nav">
-      <div className="left-sidebar-header">
+      <div className={"left-sidebar-header " + (isHover ? "test" : "")}>
         <button
           aria-label="Close Sidebar Menu"
           className="hamburger"
@@ -25,7 +36,7 @@ export const LeftSidebar = props => {
         {links.map((link, index) => {
           return (
             <li className="left-sidebar-items" key={index} onClick={closeNav}>
-              <Link to={link.url}>{link.title}</Link>
+              <Link to={link.url}><Shuffle text={link.title}/></Link>
             </li>
           )
         })}
